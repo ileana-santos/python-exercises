99a7baf2c262a04b01c7c6265a93a2e1b799fef0
Ileana Santos <124452134+ileana-santos@users.noreply.github.com>
2025-05-16 20:53:00 +0200

    Create hello_name.py

---
+++ hello_name.py
@@ -0,0 +1,10 @@
+def hello_name(name):
+"""Given a string name, return a greeting of the form "Hello name!".
+
+  Args:
+    name: The name to greet.
+
+  Returns:
+    A greeting string.
+  """
+  return "Hello " + name + "!"
